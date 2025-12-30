8fcd54dc72d38b3d2de3675a1c9a6536edc2d7cf
David David <intellectualtechnology1@gmail.com>
2025-12-30 03:33:22 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'http://yourdomain.com'],
+    origin: ['http://localhost:3000', 'https://bingo-software.onrender.com'],
     methods: ['GET', 'POST']
   },
   reconnection: true,
